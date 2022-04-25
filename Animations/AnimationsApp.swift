c3f8e51d871a7e3d0ab43f4186209a675ebf6cd1
gokhanBozkrt <99559446+gokhanBozkrt@users.noreply.github.com>
2022-04-25 22:44:59 +0300

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Gokhan Bozkurt on 25.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
